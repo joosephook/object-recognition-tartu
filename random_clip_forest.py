178007fd435cd835fac003d282454502c9141a42
joosep <joosephook@gmail.com>
2022-11-02 17:50:43 +0200

    also add hflipped image

--- random_clip_forest.py
+++ random_clip_forest.py
@@ -10,7 +10,6 @@ import pandas as pd
 import os
 import torchvision.transforms.functional as fn
 
-
 class SquarePad:
     def __call__(self, image):
         w, h = image.size
@@ -63,6 +62,36 @@ def open_img_id(img_id: str) -> ImageType:
     img.load()
     return img
 
+
+def resnet50_features():
+    from torchvision.models import resnet50, ResNet50_Weights
+    weights = ResNet50_Weights.IMAGENET1K_V2
+
+    model = resnet50(weights=weights)
+    model.eval()
+
+    t = weights.transforms()
+
+    def features(x):
+        # See note [TorchScript super()]
+        x = torch.cat([t(img).unsqueeze(0) for img in x], dim=0)
+        with torch.no_grad():
+            x = weights.transforms()(x)
+            x = model.conv1(x)
+            x = model.bn1(x)
+            x = model.relu(x)
+            x = model.maxpool(x)
+
+            x = model.layer1(x)
+            x = model.layer2(x)
+            x = model.layer3(x)
+            x = model.layer4(x)
+            return torch.sum(x, dim=(2,3)).cpu().numpy()
+
+    return features
+
+
+
 if __name__ == '__main__':
     # read in images
     df = pd.read_csv('train.csv')
@@ -70,24 +99,45 @@ if __name__ == '__main__':
     df = df.loc[df.image_id.apply(img_exists)]
     df['Images'] = df['image_id'].apply(open_img_id)
     # pad all images to a square to not lose information
-    # square_pad = SquarePad()
+    square_pad = SquarePad()
     # df['Images'] = df['Images'].apply(square_pad)
+
+    df2 = df.copy()
+    df2['Images'] = df['Images'].apply(fn.hflip)
+    df = pd.concat([df, df2])
+
+    print(df.shape)
+    print(df.shape)
 
     # read in labels
     labelsdf = pd.read_csv('labels.csv')
     labels = labelsdf['object'].values.tolist()
     clip_model = CLIP()
+    resnet50 = resnet50_features()
 
     img_embeddings = clip_model.img_embeddings(df['Images'])
     X = img_embeddings
+    # X_ = resnet50(df['Images'])
+    # X = np.hstack((X, X_))
+    # X = X_
+    # print(X_.shape)
+
+
+
     y = np.array([
         onehot(lbl) for lbl in df['labels']
     ]).astype(int)
 
-    from sklearn.multioutput import MultiOutputClassifier
+    from sklearn.multioutput import MultiOutputClassifier, ClassifierChain
     from sklearn.linear_model import LogisticRegression
-    rf = MultiOutputClassifier(LogisticRegression(dual=True, C=1.0, solver='liblinear', class_weight='balanced', max_iter=1000, random_state=2135))
+    from sklearn.preprocessing import StandardScaler
+
+    rf = ClassifierChain(LogisticRegression(dual=True, solver='liblinear', class_weight='balanced', random_state=3425))
+    sc = StandardScaler()
+    X = sc.fit_transform(X)
     rf.fit(X, y)
+
+
 
     testdf = pd.read_csv('test.csv')
     testlabels = []
@@ -99,8 +149,13 @@ if __name__ == '__main__':
 
     for img_id in testdf.image_id:
         try:
-            x = clip_model.img_embeddings(open_img_id(img_id))
-            prediction = rf.predict(x)
+            x1 = clip_model.img_embeddings(open_img_id(img_id))
+            # x1 = np.hstack((x1, resnet50([open_img_id(img_id)])))
+            x1 = sc.transform(x1)
+            x3 = clip_model.img_embeddings(fn.hflip(open_img_id(img_id)))
+            # x3 = np.hstack((x3, resnet50([open_img_id(img_id)])))
+            x3 = sc.transform(x3)
+            prediction = rf.predict(x1) + rf.predict(x3)
             predicted_labels = labelstring(prediction.astype(bool))
             # assert len(predicted_labels), img_id
 
@@ -116,7 +171,7 @@ if __name__ == '__main__':
             print(img_id)
             testlabels.append('l0')
     testdf['labels'] = testlabels
-    testdf.to_csv('joosep_submissions/clip_logistic_regression_img_embeddings_dual_formulation', index=False)
+    testdf.to_csv('joosep_submissions/clip_logistic_dual_scaled_hflip.csv', index=False)
 
 
 
