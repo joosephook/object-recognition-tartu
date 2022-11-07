dddc8029f924010eb04495b82fc215fa9c4d79bd
joosep <joosephook@gmail.com>
2022-11-07 18:20:41 +0200

    hue distribution vector

--- random_clip_forest.py
+++ random_clip_forest.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 
 import torch
 import clip
@@ -9,6 +9,8 @@ from PIL.Image import Image as ImageType
 import pandas as pd
 import os
 import torchvision.transforms.functional as fn
+from sklearn.svm import SVC
+
 
 class SquarePad:
     def __call__(self, image):
@@ -91,43 +93,106 @@ def resnet50_features():
     return features
 
 
+def set_cover(universe, subsets):
+    """Find a family of subsets that covers the universal set"""
+    elements = set(e for s in subsets for e in s)
+    # Check the subsets cover the universe
+    if elements != universe:
+        return None
+    covered = set()
+    cover = []
+    # Greedily add the subsets with the most uncovered points
+
+    used_subsets = []
+    while covered != elements:
+        dists = []
+        for i, subset in enumerate(subsets):
+            dists.append((len(subset - covered), i))
+
+        d, i = max(dists)
+        cover.append(subsets[i])
+        used_subsets.append(i)
+        covered |= subsets[i]
+
+    return used_subsets
+
+def hue_vector(imgs):
+
+    vector = []
+    for img in imgs:
+        h = np.array(img.convert('HSV'))[:,:,0].ravel()
+        counts = np.bincount(h, minlength=256)
+        vector.append(counts/counts.sum())
+    return np.array(vector)
 
 if __name__ == '__main__':
     # read in images
     df = pd.read_csv('train.csv')
     # throw away missing images
     df = df.loc[df.image_id.apply(img_exists)]
+    df = df.reset_index()
+
     df['Images'] = df['image_id'].apply(open_img_id)
-    # pad all images to a square to not lose information
-    square_pad = SquarePad()
-    # df['Images'] = df['Images'].apply(square_pad)
 
+    subsets = [
+        set(np.arange(92)[onehot(lbl).astype(bool)])
+        for lbl in df['labels']
+    ]
+    subsets_used = set_cover(set(range(92)), subsets)
 
-    print(df.shape)
-    print(df.shape)
+    val = df.index.isin(subsets_used)
+    train = ~val
+
+    train_idx = df.index[train]
+    val_idx   = df.index[val]
 
     # read in labels
     labelsdf = pd.read_csv('labels.csv')
     labels = labelsdf['object'].values.tolist()
     clip_model = CLIP()
 
-    img_embeddings = clip_model.img_embeddings(df['Images'])
-    X = (img_embeddings + clip_model.img_embeddings(df['Images'].apply(fn.hflip)))/2
+    X = clip_model.img_embeddings(df['Images'])
+    X = np.hstack((X, hue_vector(df['Images'])))
 
     y = np.array([
         onehot(lbl) for lbl in df['labels']
     ]).astype(int)
 
+    val_df = pd.read_csv('val_clean.csv')
+    val_df['Images'] = val_df['image_id'].apply(open_img_id)
+    val_X = clip_model.img_embeddings(val_df['Images'])
+    val_y = np.array([
+        onehot(lbl) for lbl in val_df['labels']
+    ]).astype(int)
+
     from sklearn.multioutput import MultiOutputClassifier, ClassifierChain
     from sklearn.linear_model import LogisticRegression
     from sklearn.preprocessing import StandardScaler
+    from sklearn.metrics import f1_score
+    from sklearn.metrics import f1_score
+    from sklearn.model_selection import GridSearchCV
+    from sklearn.pipeline import Pipeline
+    from sklearn.preprocessing import StandardScaler, MinMaxScaler
+    pipe = Pipeline(
+        [
+            ('sc', StandardScaler()),
+            ('model',
+           ClassifierChain(LogisticRegression(dual=True, solver="liblinear", random_state=3425, class_weight='balanced', max_iter=400))
+           )])
 
-    order = pd.read_csv('labels.csv')['order']
-    sc = StandardScaler()
-    sc.fit(X)
-    rf = ClassifierChain(LogisticRegression(dual=True, C=0.1, solver='liblinear', class_weight='balanced', random_state=3425), order=order)
-    X = sc.transform(X)
-    rf.fit(X, y)
+    grid = GridSearchCV(
+        pipe,
+        param_grid={
+            'model__base_estimator__C':[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+        },
+        cv=[(train_idx, val_idx)],
+        scoring='f1_macro',
+        refit=True
+    )
+    grid.fit(X, y)
+    print(grid.best_score_, grid.best_params_)
+
+    model = grid.best_estimator_
 
     testdf = pd.read_csv('test.csv')
     testlabels = []
@@ -139,11 +204,9 @@ if __name__ == '__main__':
 
     for img_id in testdf.image_id:
         try:
-            x1 = clip_model.img_embeddings(open_img_id(img_id))
-            x3 = clip_model.img_embeddings(fn.hflip(open_img_id(img_id)))
-            x = (x1+x3)/2
-            x = sc.transform(x)
-            prediction = rf.predict(x)
+            x = clip_model.img_embeddings(open_img_id(img_id))
+            x = np.hstack((x, hue_vector([open_img_id(img_id)])))
+            prediction = model.predict(x)
             predicted_labels = labelstring(prediction.astype(bool))
 
             if len(predicted_labels) == 0:
@@ -158,7 +221,7 @@ if __name__ == '__main__':
             print(img_id)
             testlabels.append('l0')
     testdf['labels'] = testlabels
-    testdf.to_csv('joosep_submissions/clip_logistic_dual_scaled_hflip_merge_strong_reg_scaled.csv', index=False)
+    testdf.to_csv('joosep_submissions/logistic_regression_hue.csv', index=False)
 
 
 
