039476617c49dd89f1efc09606b74e69327a3ddd
joosep <joosephook@gmail.com>
2022-11-08 16:00:10 +0200

    fix some stuf

--- random_clip_forest.py
+++ random_clip_forest.py
@@ -12,7 +12,9 @@ from sklearn.decomposition import PCA
 from sklearn.feature_selection import SelectKBest
 from sklearn.linear_model import LogisticRegression
 from sklearn.multioutput import ClassifierChain
+from sklearn.naive_bayes import GaussianNB, ComplementNB
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
 
 
 class SquarePad:
@@ -24,15 +26,18 @@ class SquarePad:
         padding = (hp, vp, hp, vp)
         return fn.pad(image, padding, 0, 'constant')
 
+
 def img_exists(img_id):
     exists = os.path.isfile(os.path.join('images', img_id))
     return exists
 
+
 def onehot(labels: str) -> np.ndarray:
     labels_int = list(map(int, labels.replace('l', '').split(' ')))
-    labels = np.zeros(92)
-    labels[labels_int] = 1.0
+    labels = np.zeros(92, dtype=int)
+    labels[labels_int] = 1
     return labels
+
 
 def labelstring(onehot: np.ndarray) -> str:
     labels = np.array([f'l{i}' for i in range(92)])
@@ -59,6 +64,7 @@ class CLIP:
         with torch.no_grad():
             img_embeddings = self.model.encode_image(images)
         return img_embeddings.cpu().numpy()
+
 
 def open_img_id(img_id: str) -> ImageType:
     img = Image.open(os.path.join('images', img_id))
@@ -89,7 +95,7 @@ def resnet50_features():
             x = model.layer2(x)
             x = model.layer3(x)
             x = model.layer4(x)
-            return torch.sum(x, dim=(2,3)).cpu().numpy()
+            return torch.sum(x, dim=(2, 3)).cpu().numpy()
 
     return features
 
@@ -117,106 +123,131 @@ def set_cover(universe, subsets):
 
     return used_subsets
 
-def hue_vector(imgs):
 
+def hue_vector(imgs):
     vector = []
     for img in imgs:
-        h = np.array(img.convert('HSV'))[:,:,0].ravel()
+        h = np.array(img.convert('HSV'))[:, :, 0].ravel()
         counts = np.bincount(h, minlength=256)
-        vector.append(counts/counts.sum())
+        vector.append(counts / counts.sum())
     return np.array(vector)
 
+
+def features(imgs):
+    clip_model = CLIP()
+    details = pd.read_csv('labels_detailed.csv')['object']
+    # return np.hstack(
+    #     (
+    #         clip_model(imgs, details),
+    #         clip_model.img_embeddings(imgs)
+    #      )
+    # )
+    return clip_model(imgs, details)
+    # return clip_model.img_embeddings(imgs)
+
+
+def generate_split(df):
+    y_oh = np.array([onehot(lbl) for lbl in df['labels']]).astype(int)
+    subsets_available = [
+        set(np.arange(92)[lbl.astype(bool)]) for lbl in y_oh
+    ]
+    subsets = set_cover(set(range(92)), subsets_available)
+
+    val = df.index.isin(subsets)
+    train = ~val
+
+    train_idx = df.index[train]
+    val_idx = df.index[val]
+
+    yval = y_oh[val_idx]
+    ytrain = y_oh[train_idx]
+
+    assert np.all(yval.sum(axis=0) > 0) and np.all(ytrain.sum(axis=0) > 0)
+
+    return [(train_idx, val_idx)]
+
+
+def label_test_set(m):
+    testdf = pd.read_csv('test.csv')
+    testlabels = []
+    labelsdf = pd.read_csv('labels.csv')
+    for img_id in testdf.image_id:
+        try:
+            x = features([open_img_id(img_id)])
+            prediction = m.predict(x)
+            predicted_labels = labelstring(prediction.astype(bool))
+
+            if len(predicted_labels) == 0:
+                testlabels.append('l1')
+            else:
+                testlabels.append(predicted_labels)
+            print(img_id,
+                  ' '.join(labelsdf.loc[labelsdf.label_id.isin(testlabels[-1].split(' ')), 'object'].values.ravel()),
+                  sep='\t')
+        except FileNotFoundError:
+            print(img_id, 'missing, defaulting to l0')
+            testlabels.append('l0')
+
+    testdf['labels'] = testlabels
+    import time
+    timestamp = int(time.time())
+    import os
+    import shutil
+    outdir = f'joosep_submissions/{timestamp}'
+    os.mkdir(outdir)
+    testdf.to_csv(os.path.join(outdir, f'{timestamp}_test.csv'), index=False)
+    shutil.copy(__file__, outdir)
 
 
 if __name__ == '__main__':
     # read in images
     df = pd.read_csv('train.csv')
     # throw away missing images
-    df = df.loc[df.image_id.apply(img_exists)]
-    df = df.reset_index()
-
+    df = df.loc[df.image_id.apply(img_exists)].reset_index()
     df['Images'] = df['image_id'].apply(open_img_id)
 
-    def generate_splits(n):
-        subsets = [
-            set(np.arange(92)[onehot(lbl).astype(bool)])
-            for lbl in df['labels']
-        ]
-        from itertools import permutations
-
-        subsets_used = []
-        splits = []
-        for p, _ in zip(permutations(subsets), range(n)):
-            subsets = set_cover(set(range(92)), p)
-
-            val = df.index.isin(subsets)
-            train = ~val
-
-            train_idx = df.index[train]
-            val_idx   = df.index[val]
-            if subsets not in subsets_used:
-                subsets_used.append(subsets_used)
-                splits.append((train_idx, val_idx))
-
-        return splits
-
-    cv = generate_splits(10)
-
-    # read in labels
-    labelsdf = pd.read_csv('labels.csv')
-    labels = labelsdf['object'].values.tolist()
-    clip_model = CLIP()
-    details = pd.read_csv('labels_detailed.csv')['object']
-
-    def features(imgs):
-        # return np.hstack(
-        #     (
-        #         clip_model(imgs, details),
-        #         clip_model.img_embeddings(imgs)
-        #      )
-        # )
-        # return clip_model(imgs, details)
-        return clip_model.img_embeddings(imgs)
     X = features(df['Images'])
-
-    y = np.array([
-        onehot(lbl) for lbl in df['labels']
-    ]).astype(int)
-
-    val_df = pd.read_csv('val_clean.csv')
-    val_df['Images'] = val_df['image_id'].apply(open_img_id)
-    # val_X = clip_model.img_embeddings(val_df['Images'])
-    # val_X = rn50(val_df['Images'])
-    val_X = features(df['Images'])
-
-    val_y = np.array([
-        onehot(lbl) for lbl in val_df['labels']
-    ]).astype(int)
+    y = np.vstack(df['labels'].apply(onehot).values)
 
     from sklearn.preprocessing import StandardScaler
     from sklearn.metrics import f1_score
     from sklearn.model_selection import GridSearchCV
     from sklearn.pipeline import Pipeline
     from sklearn.preprocessing import StandardScaler, MinMaxScaler
+    from sklearn.ensemble import StackingClassifier, RandomForestClassifier, VotingClassifier
     from ml_xgboost import MultiLabelXGBClassifier
+
+    mdl  = ClassifierChain(
+                VotingClassifier(
+                    estimators=[
+                        ('logdual',
+                         LogisticRegression(dual=True, solver='liblinear', random_state=342985,
+                                            class_weight='balanced')),
+                        ('knn', KNeighborsClassifier(n_neighbors=3, metric='cosine')),
+                        ('svc', SVC(class_weight='balanced', random_state=23845)),
+                        ('log', LogisticRegression(random_state=342985, class_weight='balanced')),
+                    ],
+                )
+            )
+
     pipe = Pipeline(
         [
-            ('sc', StandardScaler()),
-            # ('pca', PCA(n_components=128, whiten=True)),
-            # ('kbest', SelectKBest(k=512)),
+            ('sc', MinMaxScaler()),
             # ('model', MultiLabelXGBClassifier())
-            # ('model', ClassifierChain(LogisticRegression(dual=True, solver='liblinear', fit_intercept=True, random_state=342985, class_weight='balanced')))
-            ('model', ClassifierChain(KNeighborsClassifier(n_neighbors=3, algorithm='brute')))
-        ])
+            ('model', mdl),
+        ]
+    )
 
+    cv = generate_split(df)
+    print('generated', len(cv), 'folds')
     grid = GridSearchCV(
         pipe,
         param_grid={
             # 'model__base_estimator__C':[0.1, 0.5, 0.8, 1.0],
             # 'model__base_estimator__penalty': ['l2'],
 
-            'model__base_estimator__metric': ['euclidean', 'cosine'],
-            'model__base_estimator__weights': ['distance'],
+            # 'model__base_estimator__metric': ['euclidean', 'cosine'],
+            # 'model__base_estimator__weights': ['distance'],
 
             # 'model__n_estimators':[50],
             # 'model__booster':['gbtree'],
@@ -235,43 +266,6 @@ if __name__ == '__main__':
     grid.fit(X, y)
 
     model = grid.best_estimator_
+    label_test_set(model)
 
-    testdf = pd.read_csv('test.csv')
-    testlabels = []
-
-    # TODO: sometimes, we get NO predictions from the random forest. Investigate why.
-    # TODO: try other classifiers like SVMs with the MultiClassifier strategy from sklearn.
-    # TODO: class weights should be specified as the data is skewed as shit
-    # TODO: contact the contact person as images are missing from both the training and test sets
-
-    for img_id in testdf.image_id:
-        try:
-            # x = clip_model.img_embeddings(open_img_id(img_id))
-            x = features([open_img_id(img_id)])
-            prediction = model.predict(x)
-            predicted_labels = labelstring(prediction.astype(bool))
-
-            if len(predicted_labels) == 0:
-                testlabels.append('l1')
-            else:
-                testlabels.append(predicted_labels)
-            print(img_id, ' '.join(labelsdf.loc[labelsdf.label_id.isin(testlabels[-1].split(' ')), 'object'].values.ravel()), sep='\t')
-        except FileNotFoundError:
-            print(img_id, 'missing, defaulting to l0')
-            testlabels.append('l0')
-
-    testdf['labels'] = testlabels
-    import time
-    timestamp = int(time.time())
-    import os
-    import shutil
-    outdir = f'joosep_submissions/{timestamp}'
-    os.mkdir(outdir)
-    testdf.to_csv(os.path.join(outdir, 'test.csv'), index=False)
-    shutil.copy(__file__, outdir)
     print(grid.best_score_, grid.best_params_)
-
-
-
-
-
