0e4a73ba8a749baa0749d1c039b563bc1935b65c
joosep <joosep@find.fashion>
2022-12-03 12:34:42 +0200

    f-scores with kea_train

--- model_per_label.py
+++ model_per_label.py
@@ -1,4 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.metrics import f1_score
 
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
@@ -17,11 +20,17 @@ from sklearn.preprocessing import StandardScaler
 if __name__ == '__main__':
     padder  = SquarePad()
     # read in images
-    df = pd.read_csv('train_Kea.csv')
+    df = pd.read_csv('train.csv')
     # throw away missing images
     df = df.loc[df.image_id.apply(img_exists)].reset_index()
     df['Images'] = df['image_id'].apply(open_img_id).apply(padder)
     y = np.vstack(df['labels'].apply(onehot).values)
+
+    extra = pd.read_csv('train_Kea.csv')[['image_id', 'labels']]
+    extra = extra.loc[extra.image_id.apply(img_exists)].reset_index()
+    extra['Images'] = extra['image_id'].apply(open_img_id).apply(padder)
+    n_total = len(extra) + len(df)
+    extra = extra.iloc[~extra.index.isin(df.index)]
 
     models = []
 
@@ -39,6 +48,9 @@ if __name__ == '__main__':
     ]
     features = CLIP()
 
+    cv = generate_split(df)
+    train, val = cv[0]
+
     scores = []
     for i in range(92):
         negative = df.iloc[df.index[y[:, i] == 0]]
@@ -53,27 +65,38 @@ if __name__ == '__main__':
             new_positives.append(new_pos)
 
         augmented = pd.concat([
-            negative,
-            positive,
+            df,
+            extra,
             *new_positives
         ])
+        augmented.reset_index(inplace=True)
+        val = augmented.index.isin(cv[0][1])
+        train = ~val
+        train_idx = augmented.index[train]
+        val_idx = augmented.index[val]
+        cv = [(train_idx, val_idx)]
 
         Xs = features(augmented['Images'].tolist())
         ys = np.vstack(augmented['labels'].apply(onehot).values)
         model = LogisticRegression(solver='saga', max_iter=400, class_weight='balanced', random_state=1234)
-        # model = RidgeClassifier(alpha=0.5, solver='saga', max_iter=400, class_weight='balanced', random_state=1234)
         pipe = Pipeline([
             ('sc', StandardScaler()),
             ('model', model)
 
         ])
-        pipe.fit(Xs, ys[:,i])
-        models.append(pipe)
-        scores.append(pipe)
+        grid = GridSearchCV(pipe,
+                            {},
+                            scoring='f1_macro',
+                            n_jobs=-1,refit=True, cv=cv)
+        grid.fit(Xs, ys[:,i])
+        models.append(grid.best_estimator_)
+        scores.append(grid.best_score_)
 
     testdf = pd.read_csv('test.csv')
     testlabels = []
     labelsdf = pd.read_csv('labels.csv')
+    for lbl, score in zip(labelsdf['object'], scores):
+        print(lbl, score)
     for img_id in testdf.image_id:
         try:
             x = features([padder(open_img_id(img_id))])
